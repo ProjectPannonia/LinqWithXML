16abef542b392fcea1170ee850b5f34b839aaf77
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-03-27 12:24:55 +0200

    XML extended

--- LinqWithXML/Program.cs
+++ LinqWithXML/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LinqWithXML
 {
@@ -16,17 +17,59 @@ namespace LinqWithXML
                         <Name>Toni</Name>
                         <Age>21</Age>
                         <University>Yale</University>
+                        <YearOfStart>2022</YearOfStart>
                     </Student>
                     <Student>
                         <Name>Carla</Name>
                         <Age>17</Age>
                         <University>Yale</University>
+                        <YearOfStart>2021</YearOfStart>
                     </Student>
                     <Student>
                         <Name>Leyla</Name>
                         <Age>19</Age>
                         <University>Beijing Tech</University>
-                    </Student>";
+                        <YearOfStart>2020</YearOfStart>
+                    </Student>
+                    <Student>
+                        <Name>Adam</Name>
+                        <Age>32</Age>
+                        <University>BME</University>
+                        <YearOfStart>2022</YearOfStart>
+                    </Student>
+                </Students>";
+
+            XDocument studentsXdoc = new XDocument();
+            studentsXdoc = XDocument.Parse(studentsXML);
+
+            var students = from student in studentsXdoc.Descendants("Student")
+                           select new
+                           {
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value,
+                               YearOfStart = student.Element("YearOfStart").Value
+                           };
+
+            foreach(var student in students)
+            {
+                Console.WriteLine("Student {0} with age {1} from University {2} started in the year of {3}", 
+                                    student.Name, student.Age, student.University, student.YearOfStart);
+            }
+            // from i in someInt orderby i select i
+            var studentsSorted = from student in students 
+                                 orderby student.Age 
+                                 select student;
+
+            Console.WriteLine("Sorted students by age: ");
+
+            foreach(var student in studentsSorted)
+            {
+                Console.WriteLine("Student {0} with age {1} from University {2}", student.Name, student.Age, student.University);
+            } 
+
+            Console.ReadLine();
+
         }
     }
 }
