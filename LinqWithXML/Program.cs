a9447a737486c6e2f97273357f57b80976f2d8d6
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-03-27 11:00:31 +0200

    Add project files.

---
+++ LinqWithXML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWithXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentsXML =
+                @"<Students>
+                    <Student>
+                        <Name>Toni</Name>
+                        <Age>21</Age>
+                        <University>Yale</University>
+                    </Student>
+                    <Student>
+                        <Name>Carla</Name>
+                        <Age>17</Age>
+                        <University>Yale</University>
+                    </Student>
+                    <Student>
+                        <Name>Leyla</Name>
+                        <Age>19</Age>
+                        <University>Beijing Tech</University>
+                    </Student>";
+        }
+    }
+}
